b896e241a7ea2c16d81b02cb8aa5e433f501145c
Francesco Carlucci <francesco.carlucci6@studio.unibo.it>
2025-06-22 19:18:29 +0200

    update BasicThermometer

--- src/Thermometer.Core/BasicThermometer.cs
+++ src/Thermometer.Core/BasicThermometer.cs
@@ -6,29 +6,17 @@ namespace Thermometer.Core
 {
     public class BasicThermometer
     {
-        private readonly Random _random = new();
-        private readonly Timer _timer;
-
         public string Name { get; } = Environment.GetEnvironmentVariable("NAME") ?? "Thermometer-01";
-
+        public string id { get; } = "328122790945";
         public double ActualTemperature { get; private set; } = 20.0;
         public double RequiredTemperature { get; private set; } = 20.0;
 
-        public BasicThermometer()
-        {
-            int interval = _random.Next(20, 61) * 1000;
-            _timer = new Timer(UpdateTemperature, null, interval, interval);
-        }
-
-        private void UpdateTemperature(object? state)
+        public void SimulateTemperatureStep()
         {
             if (Math.Abs(ActualTemperature - RequiredTemperature) < 0.1)
                 return;
 
             ActualTemperature += ActualTemperature < RequiredTemperature ? 0.5 : 0.5;
-
-            int nextInterval = _random.Next(20, 61) * 1000;
-            _timer.Change(nextInterval, Timeout.Infinite);
         }
 
         public void SetRequiredTemperature(double temperature)
